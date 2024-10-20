01fa005028845e2aabed05fdada273cd3e59dd15
Denis Ten <32342205+killakazzak@users.noreply.github.com>
2024-10-20 23:21:21 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -21,7 +21,7 @@ data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_image_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
+  name        = local.vm_web_name_full
   platform_id = var.vm_web_platform_id
   resources {
     cores         = var.vm_web_cores
@@ -49,7 +49,7 @@ resource "yandex_compute_instance" "platform" {
 }
 
 resource "yandex_compute_instance" "db" {
-  name        = var.vm_db_name
+  name        = local.vm_db_name_full
   platform_id = var.vm_db_platform_id
   zone        = var.vm_db_default_zone 
   resources {
